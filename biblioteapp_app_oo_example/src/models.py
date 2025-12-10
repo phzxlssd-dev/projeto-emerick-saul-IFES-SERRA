16fdc11e94b6afa069bf6a6105bf65245bdd1e77
phzxlssd-dev <nnascimentop@gmail.com>
2025-12-10 00:26:05 -0300

    Update models.py

--- biblioteapp_app_oo_example/src/models.py
+++ biblioteapp_app_oo_example/src/models.py
@@ -1,10 +1,29 @@
-class Pessoa:
-    def __init__(self, nome, email, tipo, status="Disponível",id=None):
+class Usuario:
+    def init(self, nome, email, tipo, id=None):
         self.id = id
         self.nome = nome
         self.email = email
-        self.tipo = tipo
-        self.status = status
+        self.tipo = tipo  
 
-    def __str__(self):
-        return f"{self.nome} - {self.email} - {self.id} ({self.status})"
+    def str(self):
+        return f"{self.nome} ({self.tipo})"
+
+
+class Aluno(Usuario):
+    def init(self, nome, email, id=None):
+        super().init(nome, email, "aluno", id=id)
+
+
+class Professor(Usuario):
+    def init(self, nome, email, id=None):
+        super().init(nome, email, "professor", id=id)
+
+
+class Resultado:
+    def init(self, aluno_nome, arquivo_csv, id=None):
+        self.id = id
+        self.aluno_nome = aluno_nome
+        self.arquivo_csv = arquivo_csv  
+
+    def str(self):
+        return f"Resultado de {self.aluno_nome}"
